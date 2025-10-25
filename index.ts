f8f3feba33c076cea0602a9065a91a5dc63ddd9a
Kimmo Lepola <kimmo.lepola@gmail.com>
2025-10-25 20:43:15 +0300

    dotenv

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import { run } from "./service/service";
 import { handleNewId, handleRemoveId } from "./service/objects";
 import * as globals from "./globals";
 import { startIntervals } from "./service/intervals";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const onChannelsChanged = (peerId: string) => {
   const client = globals.clients.map[peerId];
